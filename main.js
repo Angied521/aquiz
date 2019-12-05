48ab0988c9871167528da2390aeb719537cf3772
Angie DePasquale <angied521@yahoo.com>
2019-12-05 12:04:39 -0500

    timer changes

--- main.js
+++ main.js
@@ -24,122 +24,125 @@ let questions = [
   },
 ];
 
-$('document').ready(function(){
+$('document').ready(function () {
 
- $('.start').click(function(e){
-  e.preventDefault();
-  $('.start').hide();
-  $('.quiz').show();
-  showQuestion();
- });
+  $('.start').click(function (e) {
+    e.preventDefault();
+    $('.start').hide();
+    $('.quiz').show();
+    setInterval(doUpdate, 1000);
+    showQuestion();
+  });
 
- $('.quiz ul').on('click', 'li',function(){
-   $('.selected').removeClass('selected');
-   $(this).addClass('selected');
- });
+  $('.quiz ul').on('click', 'li', function () {
+    $('.selected').removeClass('selected');
+    $(this).addClass('selected');
+  });
 
- $('.quiz a').click(function(e){
-  e.preventDefault();
-  if($('li.selected').length){
-    let guess = parseInt($('li.selected').attr('id'));
-    checkAnswer(guess);
-  } else {
-    alert('Please select an answer');
-  }    
-});
+  $('.quiz a').click(function (e) {
+    e.preventDefault();
+    if ($('li.selected').length) {
+      let guess = parseInt($('li.selected').attr('id'));
+      checkAnswer(guess);
+    } else {
+      alert('Please select an answer');
+    }
+  });
 
-$('.summary a').click(function(e){
-   e.preventDefault();
-   restartQuiz();
+  $('.summary a').click(function (e) {
+    e.preventDefault();
+    restartQuiz();
 
-});
+  });
 
-});
 
-function showQuestion(){
-  let question = questions[currentQuestion];
-  $('.quiz h2').text(question.title);
-  $('.quiz ul').html('');
-  for(var i=0; i<question.answers.length; i++){
-    $('.quiz ul').append("<li id='"+i+"'>"+question.answers[i]+"</li>");
+
+  function showQuestion() {
+    let question = questions[currentQuestion];
+    $('.quiz h2').text(question.title);
+    $('.quiz ul').html('');
+    for (var i = 0; i < question.answers.length; i++) {
+      $('.quiz ul').append("<li id='" + i + "'>" + question.answers[i] + "</li>");
+    }
   }
-}
 
-function checkAnswer(guess){
-  let question = questions[currentQuestion];
-  if(question.correct === guess){
-    score++;
+  function checkAnswer(guess) {
+    let question = questions[currentQuestion];
+    if (question.correct === guess) {
+      score++;
+    }
+    currentQuestion++;
+    if (currentQuestion >= questions.length) {
+      showSummary();
+     
+    } else {
+      count += count - 15;
+      setInterval(doUpdate, 1000);
+      showQuestion();
+    }
+
   }
-  currentQuestion++;
-  if(currentQuestion >= questions.length){
-    showSummary();
-  } else {
-  showQuestion();
-}
+  function showSummary() {
+    $('.quiz').hide();
+    $('.summary').show();
+    $('.summary h2').text("Congrats you scored " + score + "out of " + questions.length + " correct!");
+  }
 
-}
-function showSummary(){
-  $('.quiz').hide();
-  $('.summary').show();
-  $('.summary h2').text("Congrats you scored " + score + "out of " + questions.length + " correct!");
-}
+  function restartQuiz() {
+    $('.summary').hide();
+    $('.quiz').show();
+    score = 0;
+    currentQuestion = 0;
+    showQuestion();
+  }
 
-function restartQuiz(){
-  $('.summary').hide();
-  $('.quiz').show();
-  score = 0;
-  currentQuestion = 0;
-  showQuestion();
-}
+  //store Score in local storage
+  const saveScorebtn = document.getElementById("saveScoreBtn");
+  let scores = document.getElementById("score");
+  const mostRecentScore = localStorage.getItem("mostRecentStore");
 
-//store Score in local storage
-const saveScorebtn = document.getElementById("saveScoreBtn");
-let scores = document.getElementById("score");
-const mostRecentScore = localStorage.getItem("mostRecentStore");
+  const highScore = JSON.parse(localStorage.getItem("highScore")) || [];
 
-const highScore = JSON.parse(localStorage.getItem("highScore")) || [];
+  const MAX_HIGH_SCORE = 5;
 
-const MAX_HIGH_SCORE = 5;
+  scores.innerHTML = mostRecentScore;
 
-scores.innerHTML = mostRecentScore;
+  scores = e => {
+    console.log("clicked the save button!");
+    e.preventDefault();
 
-scores = e => {
-  console.log("clicked the save button!");
-  e.preventDefault();
+    const score = {
+      score: Math.floor(Math.random() * 100)
+    };
+    highScore.push(score);
+    highScore.sort((a, b) => b.score - a.score);
+    highScore.splice(5);
 
-  const score = {
-    score: Math.floor(Math.random() * 100)
-};
-highScore.push(score);
-highScore.sort ((a,b) => b.score - a.score);
-highScore.splice(5);
+    localStorage.setItem("highScore", JSON.stringify(highScore));
+    window.location.assign('/');
+  };
 
-localStorage.setItem("highScore", JSON.stringify(highScore));
-window.location.assign('/');
-};
+  // score
 
-// score
-
-const highScoreList = document.getElementById("highScore");
-const highScores = JSON.parse(localStorage.getItem("highScore")) || [];
-console.log(highScores);
+  const highScoreList = document.getElementById("highScore");
+  const highScores = JSON.parse(localStorage.getItem("highScore")) || [];
+  console.log(highScores);
 
 
 
 
-
-//timer
-$(document).ready(function() {
+var count = 0
+  //timer
 
   // count down timer counter
-  var doUpdate = function() {
-    $('.countdown').each(function() {
-      var count = parseInt($(this).html());
+  var doUpdate = function () {
+    $('.countdown').each(function () {
+       count = parseInt($(this).html());
       if (count !== 0) {
         $(this).html(count - 1);
       }
     });
   };
 
-  setInterval(doUpdate, 1000);
+  
 });
